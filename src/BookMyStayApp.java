80b3fd00ff47715a03ae69dc4e13aa71865e5730
Arihant140206 <arihantjain140602@gmail.com>
2026-03-09 08:52:57 +0530

    UC2:Hardcoded palindrome

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,62 @@
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 100);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 180);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 350);
+    }
+}
+
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        Room single = new SingleRoom();
+        Room dbl = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("=== Room Availability ===");
+
+        single.display();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        dbl.display();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.display();
+        System.out.println("Available: " + suiteAvailable);
+    }
 }
